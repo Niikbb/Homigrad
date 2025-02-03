9e94b9380058bddc3c569bf285ac5c0509b0411c
Niikbb <asd0l@yandex.ru>
2025-02-04 01:07:27 +0300

    Forcing gamemode is bo-oring.
    Более костыльной хуйни я ещё не делал. Слава богу таймер работает только один раз.
    
    Добавил cl_init'ы пустышки, просто чтоб в консоль срало меньше.

--- addons/homigrad/gamemodes/homigrad/gamemode/game/level_sh.lua
+++ addons/homigrad/gamemodes/homigrad/gamemode/game/level_sh.lua
@@ -3,11 +3,7 @@ LevelList = {}
 function TableRound(name) return _G[name or roundActiveName] end
 
 timer.Simple(0,function()
-    if roundActiveName == nil and string.find(string.lower(game.GetMap()), "rp_lone_pine") then
-        roundActiveName = "ww2"
-        roundActiveNameNext = "ww2"
-        StartRound()
-    elseif roundActiveName == nil and not string.find(string.lower(game.GetMap()), "rp_lone_pine") then
+    if roundActiveName == nil then
         roundActiveName = "homicide"
         roundActiveNameNext = "homicide"
         StartRound()
@@ -15,8 +11,11 @@ timer.Simple(0,function()
 end)
 
 timer.Simple(0,function()
-    if string.find(string.lower(game.GetMap()), "rp_lone_pine") then
+    if string.find(string.lower(game.GetMap()), "rp_lone_pine") then -- такая хуйня чтоб режим форсировать это ужас
         print("На данной карте форсирован режим - World War 2")
+        roundActiveName = "ww2"
         roundActiveNameNext = "ww2"
+        table.Empty(LevelList)
+        table.insert(LevelList,"ww2")
     end
 end)
